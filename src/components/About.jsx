e7fc8c8e8e22d8b5dd240a88e05765a3b85e54c2
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-17 18:09:10 -0500

    Add container div for animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,21 +26,23 @@ const About = () => {
   };
 
   return (
-    <motion.section
-      id="about"
-      className="min-h-[50dvh] flex flex-col justify-center gap-8"
-      initial="initial"
-      whileInView="animate"
-      variants={containerVariants}
-    >
-      <motion.h1 variants={textVariants}>About us</motion.h1>
+    <section id="about">
+      <motion.div
+        className="min-h-[50dvh] flex flex-col justify-center gap-8"
+        initial="initial"
+        whileInView="animate"
+        variants={containerVariants}
+      >
+        <motion.h1 variants={textVariants}>About us</motion.h1>
 
-      <motion.p variants={textVariants}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quam
-        ipsam dolores reprehenderit quaerat iure ab vitae enim facilis sint
-        nihil sit perspiciatis nulla ullam dignissimos eveniet sed a cupiditate.
-      </motion.p>
-    </motion.section>
+        <motion.p variants={textVariants}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+          quam ipsam dolores reprehenderit quaerat iure ab vitae enim facilis
+          sint nihil sit perspiciatis nulla ullam dignissimos eveniet sed a
+          cupiditate.
+        </motion.p>
+      </motion.div>
+    </section>
   );
 };
 
