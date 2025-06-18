03e7da5a92f97564643f676aad73cc3d233072e2
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-18 15:54:22 -0500

    Add scroll to top helper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import Header from "./components/Header.jsx";
-import Hero from "./components/Hero.jsx";
+import { useEffect } from "react";
+import { motion } from "motion/react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
-import Location from "./components/Location.jsx";
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <About />
-      <Location />
-    </>
-  );
+/**
+ * Helper component that scrolls to top of page on route change
+ */
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
 
-export default App
+/**
+ * App component that returns all routes
+ */
+export const App = () => {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About condensed={false} />} />
+      </Routes>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { App, ScrollToTop } from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
