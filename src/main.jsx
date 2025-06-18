a83a60f9c44a28db6e4853dfb753c77ae7e74521
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-18 18:52:18 -0500

    Remove ScrollToTop and Header

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import Header from "./components/Header";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ScrollToTop />
-      <Header />
       <App />
     </BrowserRouter>
   </StrictMode>
