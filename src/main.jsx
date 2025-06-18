3f7f7dfe0b17200f790c4e529883ec7a0c978f31
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-18 18:09:20 -0500

    Allow header to persist through application

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App, ScrollToTop } from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { App, ScrollToTop } from "./App";
+import Header from "./components/Header";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ScrollToTop />
+      <Header />
       <App />
     </BrowserRouter>
   </StrictMode>
