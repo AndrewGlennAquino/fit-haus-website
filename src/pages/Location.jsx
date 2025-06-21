1a8a2bfc8d715fc9ab12be6036c51f8cbe4fe8dc
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-21 16:07:53 -0500

    Create full page location component

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,11 +1,15 @@
 import { useRef } from "react";
 import { motion, useInView } from "motion/react";
+import Hero from "../components/Hero";
+import tempLocationImage from "../assets/images/temp-location-image.jpg";
 
 /**
- * Google maps component for home page
+ * Condensed Google maps component for home page
  */
-const Location = () => {
+export const LocationCondensed = () => {
   const ref = useRef(null); // ref.current.isInView
+
+  const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
   /**
    * isInView is true if component is in viewprt, otherwise false
@@ -13,27 +17,25 @@ const Location = () => {
    */
   const isInView = useInView(ref, { once: true });
 
-  const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-
-  // Animation variants for text container
+  // Animation variants for container
   const containerVariants = {
     initial: {},
     animate: {
       transition: {
-        delayChildren: 0.05,
-        staggerChildren: 0.05,
+        delayChildren: 0.25,
+        staggerChildren: 0.25,
       },
     },
   };
 
   // Animation variants for header and text
   const textVariants = {
-    initial: { opacity: 0, y: 25 },
+    initial: { opacity: 0, y: 50 },
     animate: {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.15,
+        duration: 0.75,
         ease: "easeOut",
       },
     },
@@ -51,12 +53,10 @@ const Location = () => {
         animate={isInView ? "animate" : null}
         variants={containerVariants}
       >
-        <motion.h1 variants={textVariants}>Location & Contact</motion.h1>
+        <motion.h1 variants={textVariants}>Location</motion.h1>
         <motion.p variants={textVariants}>
           Calle Vallarta #40, Mazamitla, Mexico
         </motion.p>
-        <motion.p variants={textVariants}>+52 382 688 0013</motion.p>
-        <motion.p variants={textVariants}>jorgebarajas0718@yahoo.com</motion.p>
 
         <motion.div
           className="flex justify-center items-center"
@@ -72,6 +72,81 @@ const Location = () => {
         </motion.div>
       </motion.div>
     </section>
+  );
+};
+
+/**
+ * Condensed Google maps component for home page
+ */
+const Location = () => {
+  const ref = useRef(null); // ref.current.isInView
+
+  const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+  /**
+   * isInView is true if component is in viewprt, otherwise false
+   * store value in ref
+   */
+  const isInView = useInView(ref, { once: true });
+
+  // Animation variants for container
+  const containerVariants = {
+    initial: {},
+    animate: {
+      transition: {
+        delayChildren: 0.25,
+        staggerChildren: 0.25,
+      },
+    },
+  };
+
+  // Animation variants for header and text
+  const textVariants = {
+    initial: { opacity: 0, y: 50 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.75,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  return (
+    <>
+      <Hero video={false} src={tempLocationImage} stringArray={["Location"]} />
+      <section
+        id="location"
+        ref={ref}
+        className="bg-ghost h-condensed p-default flex flex-col justify-center gap-8"
+      >
+        <motion.div
+          className="flex flex-col justify-center gap-4"
+          initial="initial"
+          animate={isInView ? "animate" : null}
+          variants={containerVariants}
+        >
+          <motion.h1 variants={textVariants}>Location</motion.h1>
+          <motion.p variants={textVariants}>
+            Calle Vallarta #40, Mazamitla, Mexico
+          </motion.p>
+
+          <motion.div
+            className="flex justify-center items-center"
+            variants={textVariants}
+          >
+            <iframe
+              width="100%"
+              height="500"
+              loading="lazy"
+              allowFullScreen
+              src={`https://www.google.com/maps/embed/v1/view?zoom=19&center=19.9143%2C-103.0249&key=${key}`}
+            />
+          </motion.div>
+        </motion.div>
+      </section>
+    </>
   );
 };
 
